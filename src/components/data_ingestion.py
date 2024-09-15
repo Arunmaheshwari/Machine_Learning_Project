943a540eccc57c6f2268dd0af91597b37dd4328b
Arun Maheshwari <maheshwariarun940@gmail.com>
2024-09-15 16:47:15 -0400

    Created data transformation pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,10 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 # This class is for storing the data in specified location
 @dataclass
 class DataIngestionConfig:
@@ -54,4 +58,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
